2a6567e067c5518fc95452bd9e6f5bc1e95e58bc
11056030 <11056030@ntub.edu.tw>
2023-10-29 22:21:58 +0800

    初始介面1029

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from mysite.models import Post
+admin.site.register(Post)
 
-# Register your models here.
--- tsai213039/settings.py
+++ tsai213039/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d=@q)jr(=2#j^-5uq9_54p%33mpk@03c!uatbnus(2%wt_obmu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mysite',
+
 ]
 
 MIDDLEWARE = [
@@ -103,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hant'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Taipei'
 
 USE_I18N = True
 
